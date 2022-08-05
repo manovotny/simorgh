2d906078035036ff205e21bdaadfdf14d0d0ad84
Michael Novotny <manovotny@gmail.com>
2022-08-05 16:09:10 -0500

    Adds custom webpack moment timezone config.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-const nextConfig = {
+const MomentTimezoneInclude = require('./src/app/legacy/psammead/moment-timezone-include/src');
+
+module.exports = {
   reactStrictMode: true,
   compiler: {
     emotion: true,
   },
-};
+  webpack: config => {
+    config.plugins.push(
+      new MomentTimezoneInclude({ startYear: 2010, endYear: 2025 }),
+    );
 
-module.exports = nextConfig;
+    return config;
+  },
+};
