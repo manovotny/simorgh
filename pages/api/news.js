5d255a3abc5abde7325e175ad6289d481ddc9af8
Michael Novotny <manovotny@gmail.com>
2022-08-09 22:54:56 -0500

    Filters page data.

--- pages/api/news.js
+++ pages/api/news.js
@@ -1,13 +1,16 @@
 import news from '#data/news/frontPage/index.json';
 import ms from 'ms';
+import { filterNews } from '../../lib/filter';
 
 const handler = async (request, response) => {
+  const filteredNews = filterNews(news);
+
   response.setHeader(
     'Cache-Control',
     `public, s-maxage=${ms('1h')}, stale-while-revalidate=${ms('15m')}`,
   );
   response.status(200);
-  response.json(news);
+  response.json(filteredNews);
 
   return response;
 };
--- pages/news.jsx
+++ pages/news.jsx
@@ -1,6 +1,7 @@
 import useSWR from 'swr';
 import news from '#data/news/frontPage/index.json';
 import FrontPage from '#pages/FrontPage/FrontPage';
+import { filterNews } from '../lib/filter';
 
 const News = props => {
   const { data } = useSWR('/api/news');
@@ -12,10 +13,12 @@ const News = props => {
 export default News;
 
 export async function getStaticProps() {
+  const filteredNews = filterNews(news);
+
   return {
     props: {
       fallback: {
-        '/api/news': news,
+        '/api/news': filteredNews,
       },
     },
   };
