80dfa7657c887622965a25e06822f2a899ed2768
Michael Novotny <manovotny@gmail.com>
2022-08-13 15:16:45 -0500

    Comments out unused `OptimizelyPageViewTracking` import.

--- src/app/pages/ArticlePage/ArticlePage.jsx
+++ src/app/pages/ArticlePage/ArticlePage.jsx
@@ -40,7 +40,7 @@ import Timestamp from '#containers/ArticleTimestamp';
 import ATIAnalytics from '#containers/ATIAnalytics';
 import ChartbeatAnalytics from '#containers/ChartbeatAnalytics';
 import ComscoreAnalytics from '#containers/ComscoreAnalytics';
-import OptimizelyPageViewTracking from '#containers/OptimizelyPageViewTracking';
+// import OptimizelyPageViewTracking from '#containers/OptimizelyPageViewTracking';
 import articleMediaPlayer from '#containers/ArticleMediaPlayer';
 import LinkedData from '#containers/LinkedData';
 import MostReadContainer from '#containers/MostRead';
