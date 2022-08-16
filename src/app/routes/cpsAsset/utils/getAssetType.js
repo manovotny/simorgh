71c9f66b0594b31bf8bf59786ac85b7a474f8f61
Michael Novotny <manovotny@gmail.com>
2022-08-15 23:55:32 -0500

    Removes unnecessary files.

--- src/app/routes/cpsAsset/utils/getAssetType.js
+++
@@ -1,5 +0,0 @@
-import path from 'ramda/src/path';
-
-const getAssetType = pageData => path(['metadata', 'type'], pageData);
-
-export default getAssetType;
--- src/server/utilities/onEnvironment/index.js
+++
@@ -1,9 +0,0 @@
-const onEnvironment = (env, params) => wrappedFunction => {
-  const shouldCallFunction = env.includes(process.env.SIMORGH_APP_ENV);
-
-  if (!shouldCallFunction) return;
-
-  wrappedFunction(params);
-};
-
-export default onEnvironment;
